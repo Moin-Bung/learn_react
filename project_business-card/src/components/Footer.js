f8a50a914bd218747c7de9e088506a9e20cc1cf7
moin bunglawala <moin.bung@gmail.com>
2024-07-15 02:05:36 -0400

    added links to footer icons

--- project_business-card/src/components/Footer.js
+++ project_business-card/src/components/Footer.js
@@ -6,9 +6,15 @@ import '../App.css';
 function Footer() {
   return (
     <footer className="footer">
-      <img className="footer--links-icon" src={twitter} alt="email-icon"/> 
-      <img className="footer--links-icon" src={linkedin} alt="email-icon"/> 
-      <img className="footer--links-icon" src={github} alt="email-icon"/> 
+      <a href="https://twitter.com/balmoinparis">
+        <img className="footer--links-icon" src={twitter} alt="Twitter icon"/>
+      </a>
+      <a href="https://linkedin.com/in/moin-bung">
+        <img className="footer--links-icon" src={linkedin} alt="LinkedIn icon"/>
+      </a>
+      <a href="https://github.com/Moin-Bung">
+        <img className="footer--links-icon" src={github} alt="GitHub icon"/>
+      </a>
     </footer>
   );
 }
